0d774bb3797e914afbf0f9f47c04abfdd6cccbb5
idozemel <idozemel94@gmail.com>
2021-05-04 17:14:58 +0300

    Update

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -10,36 +10,5 @@ import java.util.Observable;
 
 public class Model extends Observable {
 
-    public class Properties
-    {
-        String type;
-        String format;
-        String node;
-
-        public Properties(String type, String format, String node) {
-            this.type = type;
-            this.format = format;
-            this.node = node;
-        }
-    }
-    Map<String,Properties> fgProperties = new HashMap<>();
-    XMLDecoder d;
-
-    {
-        try {
-            d = new XMLDecoder(
-                    new BufferedInputStream(
-                            new FileInputStream("Test.xml")));
-
-            for (int i = 0; i < 42; i++)
-            {
-                fgProperties.getOrDefault(d.readObject(),new Properties()) = d.readObject();
-
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
 
 }
---
+++ src/Model/myDecoder.java
@@ -0,0 +1,73 @@
+package Model;
+
+import java.beans.XMLDecoder;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class myDecoder {
+    public class Properties
+    {
+        String type;
+        String format;
+        String node;
+
+        public Properties(String type, String format, String node) {
+            this.type = type;
+            this.format = format;
+            this.node = node;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getFormat() {
+            return format;
+        }
+
+        public void setFormat(String format) {
+            this.format = format;
+        }
+
+        public String getNode() {
+            return node;
+        }
+
+        public void setNode(String node) {
+            this.node = node;
+        }
+        @Override
+        public String toString(){
+            return "Decoded : [1-" + type + " 2-" + format + " 3-" + node +"]";
+        }
+    }
+
+    public myDecoder() { }
+
+    Map<String,Properties> fgProperties = new HashMap<>();
+    public void ShoterDecoder() {
+        XMLDecoder d;
+        Object stam = new Object();
+        {
+            try {
+                d = new XMLDecoder(
+                        new BufferedInputStream(
+                                new FileInputStream("playback_small.xml")));
+                Properties result = (Properties) d.readObject();
+                d.close();
+                System.out.println(result);
+                //return result;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        //return stam;
+    }
+}
