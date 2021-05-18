e7b900a46da16a208e12908932e1d79b2cbba117
idozemel <idozemel94@gmail.com>
2021-05-18 14:38:02 +0300

    Update run 9.5 added buttom class, problem with viewModel constructor.

--- src/View/joystick/MyJoystick.java
+++ src/View/joystick/MyJoystick.java
@@ -1,4 +1,4 @@
-package View;
+package View.joystick;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -6,10 +6,14 @@ import javafx.scene.layout.Pane;
 
 public class MyJoystick extends Canvas {
      Canvas joystick;
+     double mx,my;
 
     void moveJoystick (){
     //Aileron X & Elevators Y
         GraphicsContext gc = joystick.getGraphicsContext2D();
+        mx = joystick.getWidth()/2;
+        my = joystick.getHeight()/2;
+        gc.clearRect(0,0,joystick.getWidth(),joystick.getHeight());
     }
 
 
