f023ac62a607d4ccc5eb19873359d2ab50dd0f09
YonatanSchuster <yonatanmsch@gmail.com>
2021-05-18 13:41:04 +0300

    kkk

--- src/View/MyJoystick.java
+++ src/View/MyJoystick.java
@@ -1,6 +1,16 @@
 package View;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 
-public class MyJoystick extends Pane {
+public class MyJoystick extends Canvas {
+     Canvas joystick;
+
+    void moveJoystick (){
+    //Aileron X & Elevators Y
+        GraphicsContext gc = joystick.getGraphicsContext2D();
+    }
+
+
 }
