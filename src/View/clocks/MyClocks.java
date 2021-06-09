c855649b1626c4b55688dd9cd0be93bb88b65c45
idozemel <idozemel94@gmail.com>
2021-06-09 06:05:13 +0300

    Clocks ba ktana

--- src/View/clocks/MyClocks.java
+++ src/View/clocks/MyClocks.java
@@ -1,4 +1,25 @@
 package View.clocks;
 
+import View.files.MyFilesController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class MyClocks {
+
+
+
+    public MyClocks() {
+        super();
+        try {
+            FXMLLoader fxl = new FXMLLoader();
+            AnchorPane ap = fxl.load(getClass().getResource("Clocks.fxml").openStream());
+            MyClocksController myCController = fxl.getController();
+            //this.getChildren().add(ap);
+
+        } catch (IOException e) { e.printStackTrace(); }
+
+
+    }
 }
