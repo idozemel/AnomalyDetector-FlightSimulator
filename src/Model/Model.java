9f992523a513b6a7aa18807cabbd20cbb838b2b5
idozemel <idozemel94@gmail.com>
2021-06-13 16:42:21 +0300

    walla XMLreader noder

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -6,9 +6,7 @@ import ViewModel.ViewModel;
 import javafx.beans.property.IntegerProperty;
 
 import java.beans.XMLDecoder;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 
 public class Model extends Observable {
@@ -73,6 +71,83 @@ public class Model extends Observable {
             timestep.set(0);
         }
         // לעשות ELSE נוסף ולבדוק שאנחנו לא בסוף הטיסה
+    }
+//------------------Eli the boged------------------------------------//
+    public int row,col =0;
+    float[][] flightData;
+    File csv;
+
+    public void connect() throws IOException, InterruptedException {
+        /*
+        PUT THIS IN FLIGHTGEAR SETTINGS
+        --generic=socket,in,10,127.0.0.1,5400,tcp,playback_small
+        --fdm=null
+         */
+
+        //fg=new Socket("localhost", 5400);
+        //in= new BufferedReader(new FileReader(csv));
+        //out=new PrintWriter(fg.getOutputStream());
+       /* String line;
+        while((line=in.readLine())!=null) {out.println(line);
+        out.flush();
+        Thread.sleep(100);}
+        out.close();
+        in.close();
+        fg.close();*/
+    }
+
+    public void initFlightData(File file) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String line = bufferedReader.readLine();
+        for (int i=0;i<line.length();i++) {
+            if (line.charAt(i) == ',') {
+                col++;
+            }
+        }
+        col++;
+        while (line!=null){
+            row++;
+            line= bufferedReader.readLine();
+        }
+
+        flightData = new float[row][col];
+
+
+    }
+
+    public void readCSV(File file) throws IOException {
+        // flightData = new float[2174][42];
+        initFlightData(file);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        int prevComma=0;
+        int countRow = 0;
+        int countCol = 0;
+        String line = bufferedReader.readLine();
+        while (line!=null){
+            for (int i=0;i<line.length();i++) {
+                if (line.charAt(i) == ',') {
+
+
+                    flightData[countRow][countCol] = Float.parseFloat(line.substring(prevComma, i));
+                    prevComma = i + 1;
+                    countCol++;
+                }
+            }
+            prevComma=0;
+            countCol=0;
+            countRow++;
+            line= bufferedReader.readLine();
+        }
+        csv = file;
+        try {
+            connect();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setChanged();
+        notifyObservers("csv");
+
     }
 
 }
